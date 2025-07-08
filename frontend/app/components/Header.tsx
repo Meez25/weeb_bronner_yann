d3e12905e276460df3f963dab040348ea4fe67fa
Yann Bronner <yann.bronner@gmail.com>
2025-07-08 16:31:16 +0200

    feat: add style

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -13,19 +13,13 @@ export const Header = () => {
           <a href="/contact" className="text-white hidden md:block self-center cursor-pointer">Contact</a>
         </div>
         <div className='hidden md:flex'>
-          <a href="/connexion" className='flex justify-center items-center text-white bg-[#9333EA] md:h-12 md:w-[156px] md:rounded-lg md:self-center'>
-            Se connecter
-          </a>
+          <a href="/connexion" className='flex justify-center items-center text-white bg-[#9333EA] md:h-12 md:w-[156px] md:rounded-lg md:self-center hover:bg-purple-500'>Se connecter</a>
         </div>
         <BurgerMenu isModalOpen={isModalOpen} setModalOpen={setModalOpen} />
       </div>
       {isModalOpen && <div className='absolute md:hidden bg-[#0F172A] top-[78px] left-0 w-full text-white cursor-pointer flex flex-col px-6 py-2 gap-2'>
-        <a href="/contact" className='cursor-pointer'>
-          Contact
-        </a>
-        <a href="/connexion" className='cursor-pointer'>
-          Se connecter
-        </a>
+        <a href="/contact" className='cursor-pointer'>Contact</a>
+        <a href="/connexion" className='cursor-pointer'>Se connecter</a>
       </div>}
     </header >
   )
