7029eb2edd5214afa77ef8200da89956f57722df
Yann Bronner <yann.bronner@gmail.com>
2025-07-09 10:47:22 +0200

    feat:improvement on the modal

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BurgerMenu } from './BurgerMenu';
 import { useState } from 'react';
 
 export const Header = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!isModalOpen) return
+
+    const checkIfClickedOutside = (e: any) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setModalOpen(false)
+      }
+    }
+    document.addEventListener("click", checkIfClickedOutside)
+    return () => {
+      document.removeEventListener("click", checkIfClickedOutside)
+    }
+  }, [isModalOpen])
 
   return (
     <header className="bg-[#0F172A] px-5 py-[15px] md:h-[144px] md:p-6">
@@ -17,7 +32,7 @@ export const Header = () => {
         </div>
         <BurgerMenu isModalOpen={isModalOpen} setModalOpen={setModalOpen} />
       </div>
-      {isModalOpen && <div className='absolute md:hidden bg-[#0F172A] top-[78px] left-0 w-full text-white cursor-pointer flex flex-col px-6 py-2 gap-2'>
+      {isModalOpen && <div ref={modalRef} className='absolute md:hidden bg-purple-500 rounded-lg top-[78px] left-0 w-full text-white cursor-pointer flex flex-col px-6 py-4 gap-4 border-purple-900 border-2'>
         <a href="/contact" className='cursor-pointer'>Contact</a>
         <a href="/connexion" className='cursor-pointer'>Se connecter</a>
       </div>}
