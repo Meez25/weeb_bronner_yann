d4687799bf6ef822e65815d4aa706264528b9e68
Yann Bronner <yann.bronner@gmail.com>
2025-11-04 16:09:40 +0100

    Integrate the API

--- frontend/app/components/BlogHero.tsx
+++ frontend/app/components/BlogHero.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import hero from '../assets/Desktop.png';
-import { Link } from 'react-router';
 
-export const Hero = () => {
+export const BlogHero = () => {
   return (
     <main className="mx-auto bg-[#0F172A] text-white font-roboto px-5 flex justify-center py-10 flex-col">
-      <h1 className="mx-auto max-w-[1000px] font-extrabold text-4xl md:text-7xl mb-10">Explorez le <span className="text-[#C084FC] font-light">Web</span> sous toutes ses <span className="underline decoration-[#C084FC] underline-offset-6">facettes</span></h1>
+      <h1 className="mx-auto max-w-[1000px] font-extrabold text-4xl md:text-7xl mb-10">Bienvenue sur notre <span className="text-[#C084FC] font-light">Blog</span></h1>
       <p className="mx-auto max-w-[1000px] font-normal mb-10 text-lg">Le monde du web évolue constamment, et nous sommes là pour vous guider à travers ses tendances, technologies et meilleures pratiques. Que vous soyez développeur, designer ou passionné du digital, notre blog vous offre du contenu de qualité pour rester à la pointe.
       </p>
       <div className="max-w-[1000px] flex flex-col mx-auto gap-2 md:gap-4 md:flex-row mb-10">
-        <Link to="/blog" className="flex items-center justify-center text-lg bg-[#9333EA] w-[237px] rounded-lg border-2 border-[#9333EA] h-12 cursor-pointer font-medium hover:bg-purple-500">
-          Découvrir les articles
-        </Link>
         <button className="text-lg border-2 rounded-lg h-12 w-[237px] cursor-pointer font-medium hover:bg-white/10">
           S'abonner à la newsletter
         </button>
       </div>
-      <img className='lg:mx-auto max-w-[1000px]' src={hero} alt="Image conceptuelle d'une interface" />
     </main>
   )
 }
--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,5 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("contact", "./routes/contact.tsx"),
-  route("connexion", "./routes/login.tsx")
+  route("connexion", "./routes/login.tsx"),
+  route("blog", "./routes/blog.tsx"),
+  route("blog/:slug", "./routes/article.tsx")
 ] satisfies RouteConfig;
