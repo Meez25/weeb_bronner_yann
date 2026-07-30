41dae991de9923e8881c71b67e00308e35f4b9b2
Yann <yann.bronner@gmail.com>
2026-07-30 13:38:22 +0200

    Add log

--- frontend/app/api/axios.ts
+++ frontend/app/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/"
+console.log("API_URL:", JSON.stringify(API_URL));
 
 const publicApi = axios.create({
   baseURL: API_URL,
